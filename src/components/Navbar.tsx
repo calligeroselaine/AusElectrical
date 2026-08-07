86bfc0048c00abd2d47289a7fcc32b5fe84f2f6f
calligeroselaine <calligeroselaine@gmail.com>
2026-08-07 19:56:49 +1000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -78,7 +78,7 @@ export default function Navbar() {
             alt={siteConfig.businessName}
             width={1283}
             height={378}
-            className="h-7 w-auto sm:h-8 md:h-9"
+            className="h-9 w-auto sm:h-10 md:h-12"
             style={{ width: "auto" }}
             priority
           />
